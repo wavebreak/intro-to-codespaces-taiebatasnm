fc0cdad38158d8df96c076d38c9e6fdcf9d98b4b
taiebatasnm <143968302+taiebatasnm@users.noreply.github.com>
2025-01-23 16:22:51 +0000

    added age for debugs

--- main.cpp
+++ main.cpp
@@ -4,8 +4,12 @@ using namespace std;
 
 int main() {
     string name;
+    int age;
     cout << "Enter your name:";
     getline(cin,name);
+    cout << "Enter your age:";
+    cin >> age;
     cout << "Hello, "<<name<<"!" << endl;
+     cout << "You are "<<age<<" years old!" << endl;
     return 0;
 }
