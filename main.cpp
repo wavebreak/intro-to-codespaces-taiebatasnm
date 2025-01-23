e03f0c66ea4b2ee527457b4bf960c248fb81d555
taiebatasnm <143968302+taiebatasnm@users.noreply.github.com>
2025-01-23 16:03:39 +0000

    sdd string input and output

--- main.cpp
+++ main.cpp
@@ -1,7 +1,11 @@
 #include <iostream>
+#include <string>
 using namespace std;
 
 int main() {
-    cout << "Hello, Codespaces!" << endl;
+    string name;
+    cout << "Enter your name:";
+    getline(cin,name);
+    cout << "Hello, "<<name<<"!" << endl;
     return 0;
 }
